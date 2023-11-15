1bae245e2408f2234a355217d01ad9ae0a212e7c
Geoffrey zimba <jayzimba40@gmail.com>
2023-11-15 04:35:28 +0200

    commit

--- screens/Registration.js
+++ screens/Registration.js
@@ -1,157 +1,223 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Alert, StyleSheet } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import axios from 'axios';
-import * as LocalAuthentication from 'expo-local-authentication';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import * as LocalAuthentication from "expo-local-authentication";
 
-function Registration() {
-  const navigation = useNavigation();
-  const [name, setName] = useState('');
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [loading, setLoading] = useState(false);
-
-  const handleRegistration = async () => {
-    if (!name || !username || !email || !password) {
-      Alert.alert('Error', 'Please fill in all the fields.');
-      return;
-    }
-
-    const emailRegex = /^\S+@\S+\.\S+$/;
-    if (!emailRegex.test(email)) {
-      Alert.alert('Error', 'Please enter a valid email address.');
-      return;
-    }
-
-    if (password.length < 8) {
-      Alert.alert('Error', 'Password must be at least 8 characters long.');
-      return;
-    }
-
-    const registrationData = {
-      name: name,
-      username: username,
-      email: email,
-      password: password,
+class Registration extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullname: "",
+      username: "",
+      email: "",
+      password: "",
+      loading: false,
     };
+  }
 
-    try {
-      setLoading(true);
+  handleRegistration = async () => {
+    // Your registration logic here
+    this.setState({
+      loading: true,
+    });
+    var fullname = this.state.fullname;
+    var username = this.state.username;
+    var email = this.state.email;
+    var password = this.state.password;
 
-      // Make a POST request to your Node.js server for user registration
-      const response = await axios.post('https://127.0.0.1:80/register', registrationData);
+    if (
+      fullname == null ||
+      username == null ||
+      email == null ||
+      password == null
+    ) {
+      Alert.alert("Incomplete form", "please provide all fields");
+    } else {
+      var formdata = new FormData();
+      formdata.append("fullname", fullname);
+      formdata.append("username", username);
+      formdata.append("email", email);
+      formdata.append("password", password);
 
-      if (response.status === 200) {
-        // Registration successful
-        Alert.alert('Registration Successful', 'You have been registered successfully.');
-        navigation.navigate('Login');
-      } else {
-        // Registration failed
-        Alert.alert('Registration Failed', `An error occurred during registration. Status: ${response.status}`);
-      }
-    } catch (error) {
-      console.error('Registration Error:', error);
-      Alert.alert('Network Error', 'An error occurred. Please check your network connection and try again.');
-    } finally {
-      setLoading(false);
+      var headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      };
+
+      var requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow",
+      };
+
+      fetch("https://www.pezabond.com/kelly/add_record.php", requestOptions)
+        .then((Response) => Response.json())
+        .then((Response) => {
+          alert(Response[0].Message);
+          if (Response[0].Message == "Added successfuly!") {
+            Alert.alert("SUCCESSFUL!!", "user added successfully");
+            this.props.navigation.navigate("Home");
+          } else if (Response[0].Message == "Already Registered") {
+            Alert.alert("FAILED!!", "pupil already existing with that id");
+          }
+        })
+        .catch((error) => {
+          console.error("ERROR:" + error);
+        })
+        .finally(() =>
+          this.setState({
+            fullname: "",
+            grade: "",
+            studentID: "",
+            loading: false,
+          })
+        );
     }
   };
 
-  const registerBiometrics = async () => {
+  registerBiometrics = async () => {
+    // Your biometrics registration logic here
     try {
       const hasHardware = await LocalAuthentication.hasHardwareAsync();
       if (!hasHardware) {
-        Alert.alert('Biometrics Not Available', 'Your device does not support biometric authentication.');
+        Alert.alert(
+          "Biometrics Not Available",
+          "Your device does not support biometric authentication."
+        );
         return;
       }
 
       const isEnrolled = await LocalAuthentication.isEnrolledAsync();
       if (!isEnrolled) {
-        Alert.alert('Biometrics Not Enrolled', 'You must enroll biometrics in your device settings.');
+        Alert.alert(
+          "Biometrics Not Enrolled",
+          "You must enroll biometrics in your device settings."
+        );
         return;
       }
 
       const result = await LocalAuthentication.authenticateAsync({
-        promptMessage: 'Place your finger on the sensor to register your biometrics.',
+        promptMessage:
+          "Place your finger on the sensor to register your biometrics.",
       });
 
       if (result.success) {
         // Here, you can send the biometric data to your server
         const biometricData = result.data;
         // Send the biometric data to your server using Axios or another HTTP library
-        Alert.alert('Biometric Enrollment', 'Your biometric data has been enrolled successfully.');
+        Alert.alert(
+          "Biometric Enrollment",
+          "Your biometric data has been enrolled successfully."
+        );
       } else {
-        Alert.alert('Biometric Enrollment Failed', 'Biometric enrollment failed. Please try again.');
+        Alert.alert(
+          "Biometric Enrollment Failed",
+          "Biometric enrollment failed. Please try again."
+        );
       }
     } catch (error) {
-      console.error('Error during biometric enrollment:', error);
-      Alert.alert('Biometric Enrollment Error', 'An error occurred during biometric enrollment. Please try again.');
+      console.error("Error during biometric enrollment:", error);
+      Alert.alert(
+        "Biometric Enrollment Error",
+        "An error occurred during biometric enrollment. Please try again."
+      );
     }
   };
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Registration</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Full Name"
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={(text) => setUsername(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email Address"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-      <View style={styles.buttonContainer}>
-        <Button title="Already Registered? Log In" onPress={() => navigation.navigate('Login')} />
-        <Button title="Add Biometric" onPress={registerBiometrics} />
-        <Button title={loading ? 'Registering...' : 'Register'} onPress={handleRegistration} disabled={loading} />
+  render() {
+    const { name, username, email, password, loading } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Registration</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Full Name"
+          value={name}
+          onChangeText={(text) => this.setState({ fullname: text })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          value={username}
+          onChangeText={(text) => this.setState({ username: text })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email Address"
+          value={email}
+          onChangeText={(text) => this.setState({ email: text })}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => this.setState({ password: text })}
+        />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.BioButton}
+            onPress={this.registerBiometrics}
+          >
+            <Text style={{ color: "#fff" }}>Biometric Registration</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.RegButton}
+            onPress={this.handleRegistration}
+          >
+            <Text>Biometric Registration</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
-    textAlign: 'center',
+    textAlign: "center",
   },
   input: {
     height: 40,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 8,
     borderRadius: 10,
   },
   buttonContainer: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    alignItems: "center",
+    justifyContent: "space-between",
     marginHorizontal: 20,
+  },
+  BioButton: {
+    backgroundColor: "#000",
+    padding: 15,
+    borderRadius: 7,
+    marginVertical: 20,
+  },
+  RegButton: {
+    borderWidth: 1,
+    borderColor: "#000",
+    padding: 10,
+    borderRadius: 7,
   },
 });
 
