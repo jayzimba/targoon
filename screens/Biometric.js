a0acbedfa3e1b0c43113db4417dbad30ea795dd2
KellyCode Mwelwa <kundamwelwa@gmail.com>
2023-11-29 12:35:09 +0200

    h

---
+++ screens/Biometric.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Biometric = () => {
+  return (
+    <View>
+      <Text>Biometric</Text>
+    </View>
+  )
+}
+
+export default Biometric
+
+const styles = StyleSheet.create({})
